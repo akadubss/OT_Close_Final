3bede21f4eb9d2a1c91e7d5002d745436526fa8c
John Weisbrodt <31863920+akadubss@users.noreply.github.com>
2017-10-12 17:08:04 -0400

    Add files via upload

--- js/parallax.js
+++ js/parallax.js
@@ -12,9 +12,9 @@ $('.parallax--bg').each(function() {
     var distanceFromBottom = offset - scroll - screenHeight
     
     if (offset > screenHeight && offset) {
-      $(this).css('background-position', 'center ' + (( distanceFromBottom  ) * 0.5) +'px');
+      $(this).css('background-position', 'center ' + (( distanceFromBottom  ) * 0.3) +'px');
     } else {
-      $(this).css('background-position', 'center ' + (( -scroll ) * 0.5) + 'px');
+      $(this).css('background-position', 'center ' + (( -scroll ) * 0.3) + 'px');
     }
   })
 }
